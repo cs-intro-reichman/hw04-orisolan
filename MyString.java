7e23ef4c4f8fec69c1d71a6aa32209e1c578de6a
ori solan <ori.solanstewart@post.runi.ac.il>
2025-11-27 20:58:48 +0200

    finall

--- MyString.java
+++ MyString.java
@@ -1,42 +1,35 @@
-/** Returns the lowercase version of the given string. */
+public class MyString {
+
+    /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        String strOut = "";
+        String out = "";
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if ((c >= 'A') && ((c <= 'Z'))) {
+            if (c >= 'A' && c <= 'Z') {
                 c = (char) (c + 32);
-                strOut = strOut + c;
-            } else {
-                strOut = strOut + c;
             }
+            out += c;
         }
-        return strOut;
+        return out;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
         if (str2.length() > str1.length()) {
             return false;
         }
-        for (int i = 0; i < str1.length(); i++) {
-            boolean check = true;
-            for (int j = 0; j < str2.length(); j++) {
-                if (i+j >= str1.length()){
-                    return false;
-                }
-                    if (str1.charAt(i + j) != str2.charAt(j)) {
-                    check = false;
 
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean match = true;
+
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
                     break;
                 }
-
-            }
-            if (check) {
-                return true;
             }
 
+            if (match) return true;
         }
 
         return false;
